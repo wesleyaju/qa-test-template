88f42ee1a11cbabb358201d5a99a8ccd264064ba
w.almeida@coodesh.com <Wesley Almeida>
2023-01-14 18:52:51 -0300

    ✨ feat: circumventing mistakes

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,19 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+
+    if (err.message.includes('angular is not defined')) {
+        return false
+    }
+
+    if (err.message.includes('Cannot read properties of undefined')) {
+        return false
+    }
+
+    if (err.message.includes("Unexpected token '<'")) {
+        return false
+    }
+
+})
