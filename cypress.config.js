282b976a722b45497fef91ecb689d90386263d95
Wesley Almeida <wesleyaju@masup.com.br>
2023-01-18 17:42:25 -0300

    🔧 chore: new cypress config and update gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '3i11dg',
+  // retries: 2,
+  defaultCommandTimeout: 10000,
+  video: true,
+  screenshotsFolder: 'cypress/snapshots',
+  videosFolder: 'cypress/videos',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
