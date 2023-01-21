2088aacf4d3f37d3edad7b9d5439b0f510a8193d
Wesley Almeida <wesleyaju@masup.com.br>
2023-01-21 14:57:21 -0300

    ✨ feat: Tests Load, Tests Smoke and Tests Stress
    Upload of load, smoke and stress test files.

---
+++ tests/signup-load.js
@@ -0,0 +1,35 @@
+import http from    'k6/http'
+import  {sleep, check} from'k6'
+import uuid from './libs/uuid.js'
+
+export const options = {
+    stages: [
+        {duration: '1m', target: 100},
+        {duration: '2m', target: 100},
+        {duration: '1m', target: 0}
+    ],
+    thresholds: {
+        http_req_duration: ['p(95)<2000'], //95% das requisições devem responder em ate 2s.
+        http_req_failed: ['rate<0.01']  //1% das requisições podem ocorrer erros.
+    }
+}
+
+export default function() { 
+    const url = 'http://localhost:3333/signup' 
+
+    const payload = JSON.stringify({email: `${uuid.v4().substring(24)}@qacademy.io`, password: 'pwd123'})
+
+    const headers = {
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const res =  http.post(url, payload, headers)
+    
+   check(res, {
+    'status should be 201': (r) => r.status === 201
+   })
+   
+    sleep(1)
+}
